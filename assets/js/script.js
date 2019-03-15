87710ef60495321c0bc853ec4e799590461e1491
Xenos Konstantinos <mr.xkon@gmail.com>
2019-03-15 04:13:56 +0200

    fix scroll to content top/bottom
     fix scroll to content top/bottom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
 ( function ( $) {
+	$.fn.wptdIsInViewport = function() {
+		var elementTop = $(this).offset().top;
+		var elementBottom = elementTop + $(this).outerHeight();
+		var viewportTop = $(window).scrollTop();
+		var viewportBottom = viewportTop + $(window).height();
+		return elementBottom > viewportTop && elementTop < viewportBottom;
+	};
+
 	$( window ).resize( function() {
 		if ( $( window ).width() < 767 ) {
 			if ( $( '.navigation-top' ).hasClass( 'site-navigation-fixed' ) ) {
@@ -8,6 +16,14 @@
 			if ( ! $( '.navigation-top' ).hasClass( 'site-navigation-fixed' ) ) {
 				$( '.navigation-top' ).addClass( 'site-navigation-fixed' );
 			}
+		}
+	} );
+
+	$( window ).scroll( function() {
+		if ( $( '.wptd-header-image' ).wptdIsInViewport() ) {
+			$( '.site-header .menu-scroll-down .icon' ).css( 'transform', 'rotate(90deg)' );
+		} else {
+			$( '.site-header .menu-scroll-down .icon' ).css( 'transform', 'rotate(-90deg)' );
 		}
 	} );
 } ( jQuery ) );
